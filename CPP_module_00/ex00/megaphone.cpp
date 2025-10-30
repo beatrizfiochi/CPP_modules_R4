f8666423d4ae4fd40bc46f9ec1661185d59eaabd
Beatriz Fiochi Soares Da Silva <bfiochi-@student.42.fr>
2025-10-30 17:41:08 +0000

    print strings

--- CPP_module_00/ex00/megaphone.cpp
+++ CPP_module_00/ex00/megaphone.cpp
@@ -6,7 +6,18 @@
 /*   By: bfiochi- <bfiochi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 14:39:55 by bfiochi-          #+#    #+#             */
-/*   Updated: 2025/10/30 14:39:56 by bfiochi-         ###   ########.fr       */
+/*   Updated: 2025/10/30 17:40:39 by bfiochi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int	main(int argc, char *argv[])
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	for (int i = 1; i < argc; ++i)
+		std::cout << argv[i];
+	std::cout << std::endl;
+	return (0);
+}
