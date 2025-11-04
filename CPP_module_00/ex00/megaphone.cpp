59068818b8579e47bc10b53e6e896dcc525dce55
Beatriz Fiochi Soares da Silva <bfiochi-@c1r8s5.42porto.com>
2025-11-04 12:07:02 +0000

    convert to uppercase

--- CPP_module_00/ex00/megaphone.cpp
+++ CPP_module_00/ex00/megaphone.cpp
@@ -6,18 +6,26 @@
 /*   By: bfiochi- <bfiochi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 14:39:55 by bfiochi-          #+#    #+#             */
-/*   Updated: 2025/10/30 17:40:39 by bfiochi-         ###   ########.fr       */
+/*   Updated: 2025/11/04 12:04:19 by bfiochi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <stdlib.h>
+#include <ctype.h>
 
 int	main(int argc, char *argv[])
 {
 	if (argc == 1)
+	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (EXIT_SUCCESS);
+	}
 	for (int i = 1; i < argc; ++i)
-		std::cout << argv[i];
+	{
+		for (int j = 0; argv[i][j] != '\0'; ++j)
+			std::cout << (char)toupper(argv[i][j]);
+	}
 	std::cout << std::endl;
-	return (0);
+	return (EXIT_SUCCESS);
 }
