15aba66c55061b95939bba0b8013f93a5c25b3a1
Beatriz Fiochi Soares Da Silva <bfiochi-@student.42.fr>
2025-11-27 17:56:17 +0000

    first steps to organize

---
+++ CPP_module_00/Delivery/ex02/Account.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bfiochi- <bfiochi-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/27 16:21:56 by bfiochi-          #+#    #+#             */
+/*   Updated: 2025/11/27 17:24:35 by bfiochi-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <iostream>
+
+// Constructor
+Account::Account(int initial_deposit)
+{
+
+}
+
+Account::~Account(void)
+{
+}
