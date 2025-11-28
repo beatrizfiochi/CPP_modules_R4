ebb123f2411e0811ffc633f174938dfed3818b2f
Beatriz Fiochi Soares Da Silva <bfiochi-@student.42.fr>
2025-11-28 12:43:28 +0000

    implement _displayTimestamp function and the constructor and destructor

--- CPP_module_00/Delivery/ex02/Account.cpp
+++ CPP_module_00/Delivery/ex02/Account.cpp
@@ -6,19 +6,60 @@
 /*   By: bfiochi- <bfiochi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 16:21:56 by bfiochi-          #+#    #+#             */
-/*   Updated: 2025/11/27 17:24:35 by bfiochi-         ###   ########.fr       */
+/*   Updated: 2025/11/28 12:42:00 by bfiochi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 #include <iostream>
+#include <ctime>
+
+// Initializing static variables
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
 
 // Constructor
-Account::Account(int initial_deposit)
+// with initializer list (non static variables)
+// syntax: constructor : variable(value of inicialization)
+Account::Account(int initial_deposit) :
+	_accountIndex(_nbAccounts++),
+	_amount(initial_deposit),
+	_nbDeposits(0),
+	_nbWithdrawals(0)
 {
-
+	_totalAmount += initial_deposit;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << checkAmount() << ";created" << std::endl;
 }
 
 Account::~Account(void)
 {
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << checkAmount() << ";closed" << std::endl;
+}
+
+int Account::checkAmount(void) const
+{
+	return (_amount);
+}
+
+// Displays a timestamp: format [YYYYMMDD_HHMMSS]
+void Account::_displayTimestamp(void)
+{
+	time_t		timestamp = time(NULL);
+	struct tm	datetime = *localtime(&timestamp);
+	char		output[16];
+
+	strftime(output, 16, "%Y%m%d_%H%M%S", &datetime);
+	std::cout << "[" << output << "] ";
+}
+
+int main(void)
+{
+    Account a(1000);
+    Account b(500);
+    Account c(42);
+	return (0);
 }
