42c5be9ca9c98bd4dee82863bc19e602b873de7d
Beatriz Fiochi Soares Da Silva <bfiochi-@student.42.fr>
2026-01-15 15:07:20 +0000

    add test to main

--- CPP_module_02/ex00/main.cpp
+++ CPP_module_02/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bfiochi- <bfiochi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 19:30:41 by bfiochi-          #+#    #+#             */
-/*   Updated: 2026/01/13 19:47:46 by bfiochi-         ###   ########.fr       */
+/*   Updated: 2026/01/15 15:06:52 by bfiochi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,8 @@ int main(void)
     std::cout << a.getRawBits() << std::endl;
     std::cout << b.getRawBits() << std::endl;
     std::cout << c.getRawBits() << std::endl;
+
+    // a.setRawBits(10);
+    // std::cout << a.getRawBits() << std::endl;
     return 0;
 }
