c37f6017b20780180ec5e3d65e3f0f214f6b4150
Beatriz Fiochi Soares da Silva <bfiochi-@c1r8s5.42porto.com>
2026-02-19 14:45:07 +0000

    .

--- CPP_module_04/ex00/WrongCat.hpp
+++ CPP_module_04/ex00/WrongCat.hpp
@@ -6,7 +6,7 @@
 /*   By: bfiochi- <bfiochi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 11:22:17 by bfiochi-          #+#    #+#             */
-/*   Updated: 2026/02/19 14:43:27 by bfiochi-         ###   ########.fr       */
+/*   Updated: 2026/02/19 14:44:52 by bfiochi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ class WrongCat : public WrongAnimal
 		WrongCat(void);
 		WrongCat(const WrongCat &copy);
 		WrongCat& operator=(const WrongCat &copy);
-		WrongCat(void);
+		~WrongCat(void);
 		void makeSound(void) const;
 };
 
